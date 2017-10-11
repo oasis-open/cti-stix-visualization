38f849107b548cc1ccf3d5e6fd2b3e7e10f4200e
Michael Chisholm <chisholm@mitre.org>
2017-10-11 13:55:58 -0400

    Rename the library "stix2-viz" and the package from "stix2jupyter" to "stix2viz".

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="stix-visualization",
+    name="stix2-viz",
     description="Visualize STIX content",
     version="1.0",
     packages=find_packages(),
     install_requires=["jupyter>=1.0.0"],
     package_data={
-        "stix2jupyter": ["stix2viz/icons/*"],
+        "stix2viz": ["stix2viz/icons/*"],
     }
 )
